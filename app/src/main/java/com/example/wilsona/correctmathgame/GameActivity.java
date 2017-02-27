83ac734c772a6089080237de259c834f69cd8880
anotherrose98 <andrewmwilson98@gmail.com>
2017-02-27 12:30:53 -0500

    gl

---
+++ app/src/main/java/com/example/wilsona/correctmathgame/GameActivity.java
@@ -0,0 +1,63 @@
+package com.example.wilsona.correctmathgame;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+    private int x;
+    private String myText;
+    private Handler myHandler;
+    private TextView myScore;
+    private TextView myDifficulty;
+    private Button randomScoreButton = (Button) findViewById(R.id.btnRandomOne);
+    private Button randomDifficultyButton = (Button) findViewById(R.id.btnRandomTwo);
+    private Button randomTextButton = (Button) findViewById(R.id.btnRandomThree);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+
+
+
+    private void generateNumber(){
+        for(int i=0; i<50; i++){
+            x = (int)(Math.random()*100+1);
+            myText = String.valueOf(x);
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            myHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                   myScore = (TextView) findViewById(R.id.txtScore);
+                   myDifficulty = (TextView) findViewById(R.id.txtDifficulty);
+                   String ogText=  myScore.getText().toString();
+                   myScore.setText(ogText+ myText);
+                   ogText= myDifficulty.getText().toString();
+                   myDifficulty.setText(ogText+myText);
+                }
+            });
+        }
+
+
+
+    }
+
+    public void doRandomOne(View view) {
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                generateNumber();
+            }
+        };
+        new Thread(myRunnable).start();
+    }
+}
--- app/src/main/java/com/example/wilsona/correctmathgame/MainActivity.java
+++ app/src/main/java/com/example/wilsona/correctmathgame/MainActivity.java
@@ -1,22 +1,52 @@
 package com.example.wilsona.correctmathgame;
 
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Handler myHandler;
+    long startTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        startTime = System.currentTimeMillis();
+
+        myHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                    long seconds = ((System.currentTimeMillis() - startTime)) / 1000;
+                    Log.i("info", "Seconds = " + seconds);
+                myHandler.sendEmptyMessageDelayed(0, 1000);
+            }
+        };
+
+        myHandler.sendEmptyMessageDelayed(0,0);
+
+        Log.i("info", "does this run?");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button play = (Button) findViewById(R.id.btnPlay);
+        play.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent i =new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
 }
